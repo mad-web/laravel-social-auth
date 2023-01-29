c58871043bc582efa3639df9798de4869edb4748
mhd <117361811+mhdEurFlash123@users.noreply.github.com>
2023-01-29 15:11:34 +0300

    remove broadcasting from the events

--- src/Events/SocialEvent.php
+++ src/Events/SocialEvent.php
@@ -5,13 +5,7 @@ namespace MadWeb\SocialAuth\Events;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-abstract class SocialEvent implements ShouldBroadcast
+abstract class SocialEvent 
 {
-    /**
-     * Get the channels the event should broadcast on.
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('social');
-    }
+    
 }
